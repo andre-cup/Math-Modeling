00330c5af91c3da684eb410415f63aeb7d7177e1
cupcakebr <30611019+cupcakebr@users.noreply.github.com>
2018-06-17 21:39:16 -0300

    Métodos para resolução de EDO's por Euler.

---
+++ Q3/sistema-runge-kutta.py
@@ -0,0 +1,48 @@
+import gc
+import matplotlib.pyplot as plt
+
+x = 4
+y = 4
+t = 0
+h = 0.001
+
+vetorY = [y]
+vetorX = [x]
+tN = [t]
+
+while t <= 16:
+    fx = -0.16 * x
+    fxy = 0.08 * x * y
+    gx = 4.5 * x
+    gxy = - 0.9 * x * y
+
+    fxH = -0.16 * x + (h / 2) + 0.08 * x * y + (h / 2)
+    gxH = 4.5 * x + (h / 2) - 0.9 * x * y + (h / 2)
+
+
+    Kn11 = t + fx + fxy
+    Kn21 = t + gx + gxy
+
+    Kn12 = (t + h/2 ) + fxH * Kn11
+    Kn22 = (t + h/2) +gxH * Kn11
+
+    Kn13 = (t + h/2 ) + fxH * Kn12
+    Kn23 = (t + h/2 ) + gxH * Kn12
+
+    Kn14 = (t + h/2 ) + fx * Kn13
+    Kn24 = (t + h/2 ) + gx * Kn13
+
+    xProximo = x - h/6 * (Kn11 + (2 * Kn12) + (2 * Kn13) + Kn14)
+    yProximo = y - h/6 * (Kn21 + (2 * Kn22) + (2 * Kn23) + Kn24)
+
+    x = xProximo
+    y = yProximo
+    t += h
+
+    vetorX.append(x)
+    vetorY.append(y)
+    tN.append(t)
+
+plt.plot(tN, vetorX, markersize = 1, color = "blue")
+plt.plot(tN, vetorY,"--", markersize = 1, color = "green")
+plt.show()
